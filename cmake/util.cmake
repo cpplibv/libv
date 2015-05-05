7332eebae42aaad8b36401d8bb8d2d69e8f67818
Csaszar Matyas [Vader] <vaderhun@gmail.com>
2015-05-05 21:30:30 +0200

    Fix unix make

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -2,7 +2,7 @@ if(NOT DEFINED PROCESSOR_COUNT)
 	set(PROCESSOR_COUNT 1) # Unknown
 
 	if(NOT WIN32)
-		execute_process(COMMAND "nproc" OUTPUT_VARIABLE info)
+		execute_process(COMMAND "nproc" OUTPUT_VARIABLE PROCESSOR_COUNT)
 	else()
 		set(PROCESSOR_COUNT "$ENV{NUMBER_OF_PROCESSORS}")
 	endif()
