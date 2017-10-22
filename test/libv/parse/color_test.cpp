d6bd8487eb798ed753d609e4d41c24bbac3dd4e0
Csaszar Matyas [Vader] <vaderhun@gmail.com>
2017-10-22 19:06:36 +0200

    Add minimal test reporter and improve color test

--- test/libv/parse/color_test.cpp
+++ test/libv/parse/color_test.cpp
@@ -3,113 +3,112 @@
 #include <catch.hpp>
 
 #include <libv/parse/color.hpp>
+#include <libv/utility/approxing.hpp>
 
-using libv::parse::color;
-using libv::parse::to_color;
 
 // -------------------------------------------------------------------------------------------------
 
-void check_color(float r, float g, float b, float a, const std::string& str) {
-	const auto result = to_color(str);
+using libv::parse::color;
+using libv::parse::to_color;
+using ApproxColor = libv::vec4_t<libv::Approxing<float, -3>>;
 
-	CHECK(result.r == Approx(r).epsilon(0.001f));
-	CHECK(result.g == Approx(g).epsilon(0.001f));
-	CHECK(result.b == Approx(b).epsilon(0.001f));
-	CHECK(result.a == Approx(a).epsilon(0.001f));
-}
+#define CHECK_COLOR(R, G, B, A, STR) \
+	CHECK(ApproxColor(R, G, B, A) == to_color(STR))
 
-TEST_CASE("Color Test") {
-	check_color(1.000f, 0.000f, 0.000f, 1.000f, "red");
-	check_color(0.753f, 0.753f, 0.753f, 1.000f, "silver");
-	check_color(0.498f, 1.000f, 0.831f, 1.000f, "aquamarine");
+// -------------------------------------------------------------------------------------------------
 
-	check_color(1.000f, 0.733f, 0.800f, 1.000f, "#FBC");
-	check_color(1.000f, 0.733f, 0.800f, 0.933f, "#FBCE");
-	check_color(0.941f, 0.945f, 0.949f, 1.000f, "#F0F1F2");
-	check_color(0.941f, 0.945f, 0.949f, 0.953f, "#F0F1F2F3");
-	check_color(0.941f, 0.945f, 0.949f, 0.953f, "# F0F1F2F3");
-	check_color(0.941f, 0.945f, 0.949f, 0.953f, " # f0 F1 F2 F3");
-	check_color(0.941f, 0.945f, 0.949f, 0.953f, "#F0 F1 F2 F3");
+TEST_CASE("parse color: test everything") {
+	CHECK_COLOR(1.000f, 0.000f, 0.000f, 1.000f, "red");
+	CHECK_COLOR(0.753f, 0.753f, 0.753f, 1.000f, "silver");
+	CHECK_COLOR(0.498f, 1.000f, 0.831f, 1.000f, "aquamarine");
 
-	check_color(0.055f, 0.059f, 0.063f, 1.000f, "rgb(14,15,16)");
-	check_color(0.055f, 0.150f, 0.063f, 1.000f, "rgb(14,15%,16)");
-	check_color(0.140f, 0.154f, 0.160f, 1.000f, "rgb(14%,15.4%,16%)");
-	check_color(0.140f, 0.154f, 0.160f, 1.000f, "rgb(0.14,0.154,0.16)");
-	check_color(1.000f, 1.000f, 0.000f, 1.000f, "RGB(1.,1.,0.)");
-	check_color(0.140f, 0.154f, 0.009f, 1.000f, "rgb(.14,.154,.9%)");
-	check_color(0.140f, 0.059f, 0.063f, 1.000f, "RGB (  14 %,   15 ,16)");
+	CHECK_COLOR(1.000f, 0.733f, 0.800f, 1.000f, "#FBC");
+	CHECK_COLOR(1.000f, 0.733f, 0.800f, 0.933f, "#FBCE");
+	CHECK_COLOR(0.941f, 0.945f, 0.949f, 1.000f, "#F0F1F2");
+	CHECK_COLOR(0.941f, 0.945f, 0.949f, 0.953f, "#F0F1F2F3");
+	CHECK_COLOR(0.941f, 0.945f, 0.949f, 0.953f, "# F0F1F2F3");
+	CHECK_COLOR(0.941f, 0.945f, 0.949f, 0.953f, " # f0 F1 F2 F3");
+	CHECK_COLOR(0.941f, 0.945f, 0.949f, 0.953f, "#F0 F1 F2 F3");
 
-	check_color(0.055f, 0.059f, 0.063f, 0.170f, "rgb(14,15,16,17%)");
-	check_color(0.055f, 0.059f, 0.063f, 0.174f, "rgb(14, 15 ,16, 17.4%)");
-	check_color(0.140f, 0.154f, 0.160f, 0.170f, "rgb(14%,15.4%,16%,17%)");
-	check_color(0.140f, 0.154f, 0.160f, 0.400f, "rgb(14%,15.4%,16%,0.4)");
-	check_color(0.140f, 0.150f, 0.160f, 0.170f, "rgb ( 14%, 15%, 16 % ,17% )");
+	CHECK_COLOR(0.055f, 0.059f, 0.063f, 1.000f, "rgb(14,15,16)");
+	CHECK_COLOR(0.055f, 0.150f, 0.063f, 1.000f, "rgb(14,15%,16)");
+	CHECK_COLOR(0.140f, 0.154f, 0.160f, 1.000f, "rgb(14%,15.4%,16%)");
+	CHECK_COLOR(0.140f, 0.154f, 0.160f, 1.000f, "rgb(0.14,0.154,0.16)");
+	CHECK_COLOR(1.000f, 1.000f, 0.000f, 1.000f, "RGB(1.,1.,0.)");
+	CHECK_COLOR(0.140f, 0.154f, 0.009f, 1.000f, "rgb(.14,.154,.9%)");
+	CHECK_COLOR(0.140f, 0.059f, 0.063f, 1.000f, "RGB (  14 %,   15 ,16)");
 
-	check_color(0.055f, 0.059f, 0.063f, 0.170f, "rgba(14,15,16,17%)");
-	check_color(0.055f, 0.059f, 0.063f, 0.174f, "rgba(14, 15 ,16, 17.4%)");
-	check_color(0.140f, 0.154f, 0.160f, 0.170f, "rgba(14%,15.4%,16%,17%)");
-	check_color(0.140f, 0.154f, 0.160f, 0.400f, "RGBA(14%,15.4%,16%,0.4)");
-	check_color(0.140f, 0.150f, 0.160f, 0.170f, "rgba ( 14%, 15%, 16 % ,17% )");
+	CHECK_COLOR(0.055f, 0.059f, 0.063f, 0.170f, "rgb(14,15,16,17%)");
+	CHECK_COLOR(0.055f, 0.059f, 0.063f, 0.174f, "rgb(14, 15 ,16, 17.4%)");
+	CHECK_COLOR(0.140f, 0.154f, 0.160f, 0.170f, "rgb(14%,15.4%,16%,17%)");
+	CHECK_COLOR(0.140f, 0.154f, 0.160f, 0.400f, "rgb(14%,15.4%,16%,0.4)");
+	CHECK_COLOR(0.140f, 0.150f, 0.160f, 0.170f, "rgb ( 14%, 15%, 16 % ,17% )");
 
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "hsl(14,15%,255)");
-	check_color(0.230f, 0.184f, 0.170f, 1.000f, "hsl(14,15%,51)");
-	check_color(0.184f, 0.147f, 0.136f, 1.000f, "hsl(14°,15%,16%)");
-	check_color(0.184f, 0.147f, 0.136f, 1.000f, "hsl(14,0.15,.16)");
-	check_color(0.184f, 0.147f, 0.136f, 1.000f, "hsl(374,0.15,.16)");
-	check_color(0.184f, 0.147f, 0.136f, 1.000f, "hsl(-346,0.15,.16)");
+	CHECK_COLOR(0.055f, 0.059f, 0.063f, 0.170f, "rgba(14,15,16,17%)");
+	CHECK_COLOR(0.055f, 0.059f, 0.063f, 0.174f, "rgba(14, 15 ,16, 17.4%)");
+	CHECK_COLOR(0.140f, 0.154f, 0.160f, 0.170f, "rgba(14%,15.4%,16%,17%)");
+	CHECK_COLOR(0.140f, 0.154f, 0.160f, 0.400f, "RGBA(14%,15.4%,16%,0.4)");
+	CHECK_COLOR(0.140f, 0.150f, 0.160f, 0.170f, "rgba ( 14%, 15%, 16 % ,17% )");
 
-	check_color(0.066f, 0.061f, 0.059f, 0.170f, "hsl(14,15,16,17%)");
-	check_color(0.066f, 0.061f, 0.059f, 0.174f, "hsl(14, 15 ,16, 17.4%)");
-	check_color(0.185f, 0.147f, 0.135f, 0.170f, "hsl(14,15.4%,16%,17%)");
-	check_color(0.185f, 0.147f, 0.135f, 0.400f, "hsl(14°,15.4%,16%,0.4)");
-	check_color(0.184f, 0.147f, 0.136f, 0.170f, "hsl ( 14, 15%, 16 % ,17% )");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "hsl(14,15%,255)");
+	CHECK_COLOR(0.230f, 0.184f, 0.170f, 1.000f, "hsl(14,15%,51)");
+	CHECK_COLOR(0.184f, 0.147f, 0.136f, 1.000f, "hsl(14°,15%,16%)");
+	CHECK_COLOR(0.184f, 0.147f, 0.136f, 1.000f, "hsl(14,0.15,.16)");
+	CHECK_COLOR(0.184f, 0.147f, 0.136f, 1.000f, "hsl(374,0.15,.16)");
+	CHECK_COLOR(0.184f, 0.147f, 0.136f, 1.000f, "hsl(-346,0.15,.16)");
 
-	check_color(0.066f, 0.061f, 0.059f, 0.170f, "hsla(14,15,16,17%)");
-	check_color(0.066f, 0.061f, 0.059f, 0.174f, "hsla(14, 15 ,16, 17.4%)");
-	check_color(0.185f, 0.147f, 0.135f, 0.170f, "hsla(14°,15.4%,16%,17%)");
-	check_color(0.185f, 0.147f, 0.135f, 0.400f, "hsla(14,15.4%,16%,0.4)");
-	check_color(0.184f, 0.147f, 0.136f, 0.170f, "hsla ( 14, 15%, 16 % ,17% )");
+	CHECK_COLOR(0.066f, 0.061f, 0.059f, 0.170f, "hsl(14,15,16,17%)");
+	CHECK_COLOR(0.066f, 0.061f, 0.059f, 0.174f, "hsl(14, 15 ,16, 17.4%)");
+	CHECK_COLOR(0.185f, 0.147f, 0.135f, 0.170f, "hsl(14,15.4%,16%,17%)");
+	CHECK_COLOR(0.185f, 0.147f, 0.135f, 0.400f, "hsl(14°,15.4%,16%,0.4)");
+	CHECK_COLOR(0.184f, 0.147f, 0.136f, 0.170f, "hsl ( 14, 15%, 16 % ,17% )");
 
-	check_color(1.000f, 0.885f, 0.850f, 1.000f, "hsv(14,15%,255)");
-	check_color(0.200f, 0.177f, 0.170f, 1.000f, "hsv(14,15%,51)");
-	check_color(0.160f, 0.142f, 0.136f, 1.000f, "hsv(14°,15%,16%)");
-	check_color(0.160f, 0.142f, 0.136f, 1.000f, "hsv(14,0.15,.16)");
-	check_color(0.160f, 0.142f, 0.136f, 1.000f, "hsv(374,0.15,.16)");
-	check_color(0.160f, 0.142f, 0.136f, 1.000f, "hsv(-346,0.15,.16)");
+	CHECK_COLOR(0.066f, 0.061f, 0.059f, 0.170f, "hsla(14,15,16,17%)");
+	CHECK_COLOR(0.066f, 0.061f, 0.059f, 0.174f, "hsla(14, 15 ,16, 17.4%)");
+	CHECK_COLOR(0.185f, 0.147f, 0.135f, 0.170f, "hsla(14°,15.4%,16%,17%)");
+	CHECK_COLOR(0.185f, 0.147f, 0.135f, 0.400f, "hsla(14,15.4%,16%,0.4)");
+	CHECK_COLOR(0.184f, 0.147f, 0.136f, 0.170f, "hsla ( 14, 15%, 16 % ,17% )");
 
-	check_color(0.063f, 0.060f, 0.059f, 0.170f, "hsv(14,15,16,17%)");
-	check_color(0.063f, 0.060f, 0.059f, 0.174f, "HSV(14, 15 ,16, 17.4%)");
-	check_color(0.160f, 0.141f, 0.135f, 0.170f, "hsv(14,15.4%,16%,17%)");
-	check_color(0.160f, 0.141f, 0.135f, 0.400f, "hsv(14°,15.4%,16%,0.4)");
-	check_color(0.160f, 0.142f, 0.136f, 0.170f, "hsv ( 14, 15%, 16 % ,17% )");
+	CHECK_COLOR(1.000f, 0.885f, 0.850f, 1.000f, "hsv(14,15%,255)");
+	CHECK_COLOR(0.200f, 0.177f, 0.170f, 1.000f, "hsv(14,15%,51)");
+	CHECK_COLOR(0.160f, 0.142f, 0.136f, 1.000f, "hsv(14°,15%,16%)");
+	CHECK_COLOR(0.160f, 0.142f, 0.136f, 1.000f, "hsv(14,0.15,.16)");
+	CHECK_COLOR(0.160f, 0.142f, 0.136f, 1.000f, "hsv(374,0.15,.16)");
+	CHECK_COLOR(0.160f, 0.142f, 0.136f, 1.000f, "hsv(-346,0.15,.16)");
 
-	check_color(0.063f, 0.060f, 0.059f, 0.170f, "hsva(14,15,16,17%)");
-	check_color(0.063f, 0.060f, 0.059f, 0.174f, "hsva(14, 15 ,16, 17.4%)");
-	check_color(0.160f, 0.141f, 0.135f, 0.170f, "HSVA(14°,15.4%,16%,17%)");
-	check_color(0.160f, 0.141f, 0.135f, 0.400f, "hsva(14,15.4%,16%,0.4)");
-	check_color(0.160f, 0.142f, 0.136f, 0.170f, "hsva ( 14, 15%, 16 % ,17% )");
+	CHECK_COLOR(0.063f, 0.060f, 0.059f, 0.170f, "hsv(14,15,16,17%)");
+	CHECK_COLOR(0.063f, 0.060f, 0.059f, 0.174f, "HSV(14, 15 ,16, 17.4%)");
+	CHECK_COLOR(0.160f, 0.141f, 0.135f, 0.170f, "hsv(14,15.4%,16%,17%)");
+	CHECK_COLOR(0.160f, 0.141f, 0.135f, 0.400f, "hsv(14°,15.4%,16%,0.4)");
+	CHECK_COLOR(0.160f, 0.142f, 0.136f, 0.170f, "hsv ( 14, 15%, 16 % ,17% )");
+
+	CHECK_COLOR(0.063f, 0.060f, 0.059f, 0.170f, "hsva(14,15,16,17%)");
+	CHECK_COLOR(0.063f, 0.060f, 0.059f, 0.174f, "hsva(14, 15 ,16, 17.4%)");
+	CHECK_COLOR(0.160f, 0.141f, 0.135f, 0.170f, "HSVA(14°,15.4%,16%,17%)");
+	CHECK_COLOR(0.160f, 0.141f, 0.135f, 0.400f, "hsva(14,15.4%,16%,0.4)");
+	CHECK_COLOR(0.160f, 0.142f, 0.136f, 0.170f, "hsva ( 14, 15%, 16 % ,17% )");
 
 	// ---------------------------------------------------------------------------------------------
 
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "rgb(-.14,.154,.9%)");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "rgb(3.14,.154,.9%)");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "rgba(14%,15.4%,16%,-15.4)");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "hsl(14%,15.4%,16%)");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "hsla(14%,15.4%,16%, .3)");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "hsl (  14 %,   15 ,16)");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "F0F1F2");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "F0F1F2F3");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "14,15,16");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "14,15,16,17");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "14, 15, 16");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "14, 15, 16, 17");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "(14,15,16)");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "(14,15,16,17)");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "(14, 15, 16)");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "(14, 15, 16, 17)");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "rgb(14,15,(16,17)");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "rgb(14,15,)16,17)");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "rgb(14)");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "rgb(14,15)");
-	check_color(1.000f, 1.000f, 1.000f, 1.000f, "rgb(14,15,433)");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "rgb(-.14,.154,.9%)");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "rgb(3.14,.154,.9%)");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "rgba(14%,15.4%,16%,-15.4)");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "hsl(14%,15.4%,16%)");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "hsla(14%,15.4%,16%, .3)");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "hsl (  14 %,   15 ,16)");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "F0F1F2");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "F0F1F2F3");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "14,15,16");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "14,15,16,17");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "14, 15, 16");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "14, 15, 16, 17");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "(14,15,16)");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "(14,15,16,17)");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "(14, 15, 16)");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "(14, 15, 16, 17)");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "rgb(14,15,(16,17)");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "rgb(14,15,)16,17)");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "rgb(14)");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "rgb(14,15)");
+	CHECK_COLOR(1.000f, 1.000f, 1.000f, 1.000f, "rgb(14,15,433)");
 }
--- test/runner.cpp
+++ test/runner.cpp
@@ -1,14 +1,15 @@
 // File: %<%NAME%>%.%<%EXTENSION%>%, Created on %<%DATE%>% %<%TIME%>%, Author: %<%USER%>%
 
 #define CATCH_CONFIG_RUNNER
+#define CATCH_CONFIG_CONSOLE_WIDTH 100
 
 #include <catch.hpp>
-#include "simple_reporter.hpp"
+#include "minimal_reporter.hpp"
 
 int main(int argc, char* const argv[]) {
 	Catch::Session session; // There must be exactly one instance
 
-	session.configData().reporterNames.emplace_back("vcomp");
+	session.configData().reporterNames.emplace_back("vminimal");
 	session.configData().useColour = Catch::UseColour::Yes;
 
 	int returnCode = session.applyCommandLine(argc, argv);
