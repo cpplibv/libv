4477f207803526daa09e73c264076712de08d25a
Csaszar Matyas [Vader] <vaderhun@gmail.com>
2018-03-18 06:23:26 +0100

    wish: Add prefix to wish cmake variables

--- cmake/wish_util.cmake
+++ cmake/wish_util.cmake
@@ -2,13 +2,13 @@
 
 # --------------------------------------------------------------------------------------------------
 
-if(NOT DEFINED PROCESSOR_COUNT)
-	set(PROCESSOR_COUNT 1) # Unknown
+if(NOT DEFINED WISH_PROCESSOR_COUNT)
+	set(WISH_PROCESSOR_COUNT 1) # Unknown
 
 	if(NOT WIN32)
-		execute_process(COMMAND "nproc" OUTPUT_VARIABLE PROCESSOR_COUNT)
+		execute_process(COMMAND "nproc" OUTPUT_VARIABLE WISH_PROCESSOR_COUNT)
 	else()
-		set(PROCESSOR_COUNT "$ENV{NUMBER_OF_PROCESSORS}")
+		set(WISH_PROCESSOR_COUNT "$ENV{NUMBER_OF_PROCESSORS}")
 	endif()
 endif()
 
