f85c6f1c519512f9b4c4501d189b16c6932e4806
Csaszar Matyas [Vader] <vaderhun@gmail.com>
2018-03-31 01:10:22 +0200

    libv.utility: Add with_ec utility

---
+++ include/libv/utility/with_ec.hpp
@@ -0,0 +1,29 @@
+// File: with_ec.hpp - Created on 2018.03.29. 08:38 - Author: Vader
+
+#pragma once
+
+// std
+#include <system_error>
+#include <type_traits>
+
+
+namespace libv {
+
+// -------------------------------------------------------------------------------------------------
+
+template <typename F, typename... Args>
+auto with_ec(F&& func, Args&&... args) {
+	using Result = std::invoke_result_t<F, Args..., std::error_code&>;
+	std::error_code ec;
+	if constexpr (std::is_same_v<Result, void>) {
+		std::forward<F>(func)(std::forward<Args>(args)..., ec);
+		return ec;
+	} else {
+		Result result = std::forward<F>(func)(std::forward<Args>(args)..., ec);
+		return std::pair<Result, std::error_code>{std::forward<Result>(result), ec};
+	}
+}
+
+// -------------------------------------------------------------------------------------------------
+
+} // namespace libv
