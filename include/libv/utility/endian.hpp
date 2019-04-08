cafb3a96807d4c95844b09be6b17a25823d0d7df
Császár Mátyás [Vader] <vaderhun@gmail.com>
2019-04-08 02:30:06 +0200

    libv.utility: Replace boost endian with C++20 std::endian

--- include/libv/utility/endian.hpp
+++ include/libv/utility/endian.hpp
@@ -2,11 +2,8 @@
 
 #pragma once
 
-// ext
-// TODO P5: C++20 replace boost with standard
-#include <boost/predef/other/endian.h>
 // std
-#include <cstdint>
+#include <type_traits>
 
 
 namespace libv {
@@ -14,19 +11,11 @@ namespace libv {
 // -------------------------------------------------------------------------------------------------
 
 constexpr inline bool is_big_endian() {
-#ifdef BOOST_BIG_ENDIAN
-	return true;
-#else
-	return false;
-#endif
+	return std::endian::native == std::endian::big;
 }
 
 constexpr inline bool is_little_endian() {
-#ifdef BOOST_LITTLE_ENDIAN
-	return true;
-#else
-	return false;
-#endif
+	return std::endian::native == std::endian::little;
 }
 
 constexpr inline bool is_network_endian() {
