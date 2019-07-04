ba70e9e411fce701aebebb0587e839bf43bcd8ad
Császár Mátyás [Vader] <vaderhun@gmail.com>
2019-07-04 19:48:44 +0200

    libv.utility: Add generic_path utility

---
+++ include/libv/utility/generic_path.hpp
@@ -0,0 +1,22 @@
+// File: approxing.hpp Author: Vader Created on 2017. augusztus 16., 23:59
+
+#pragma once
+
+// std
+#include <filesystem>
+
+
+namespace libv {
+
+// -------------------------------------------------------------------------------------------------
+
+/// I just cannot be bothered anymore, give me a generic path string
+inline std::string generic_path(const std::filesystem::path& path) {
+	auto result = path.generic_string();
+	std::replace(result.begin(), result.end(), '\\', '/'); // For windows drive letters
+	return result;
+}
+
+// -------------------------------------------------------------------------------------------------
+
+} // namespace libv
