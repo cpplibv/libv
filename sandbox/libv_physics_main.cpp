1916d62a1f28297ce9972121fc705fbad4145367
Császár Mátyás [Vader] <vaderhun@gmail.com>
2019-09-08 14:11:03 +0200

    ext: Fork reactphysics3d

--- sandbox/libv_physics_main.cpp
+++ sandbox/libv_physics_main.cpp
@@ -1,15 +1,22 @@
 // File: Main.cpp, Created on 2014.04.25. at 21:23, Author: Vader
 
 // ext
-#include <reactphysics3d/body/RigidBody.h>
-#include <reactphysics3d/collision/ProxyShape.h>
-#include <reactphysics3d/collision/shapes/BoxShape.h>
-#include <reactphysics3d/collision/shapes/SphereShape.h>
-#include <reactphysics3d/engine/DynamicsWorld.h>
-#include <reactphysics3d/engine/Material.h>
-#include <reactphysics3d/mathematics/Transform.h>
-#include <reactphysics3d/mathematics/Vector3.h>
+#include <rp3d/body/RigidBody.h>
+#include <rp3d/collision/ContactManifold.h>
+#include <rp3d/collision/ProxyShape.h>
+#include <rp3d/collision/shapes/BoxShape.h>
+#include <rp3d/collision/shapes/SphereShape.h>
+#include <rp3d/constraint/ContactPoint.h>
+#include <rp3d/engine/DynamicsWorld.h>
+#include <rp3d/engine/EventListener.h>
+#include <rp3d/engine/Material.h>
+#include <rp3d/mathematics/Quaternion.h>
+#include <rp3d/mathematics/Transform.h>
+#include <rp3d/mathematics/Vector3.h>
 // libv
+#include <libv/math/angle.hpp>
+#include <libv/math/constants.hpp>
+#include <libv/math/quaternion.hpp>
 #include <libv/math/vec.hpp>
 // std
 #include <iostream>
@@ -23,48 +30,89 @@ enum Category {
 	CATEGORY3 = 0x0004,
 };
 
-int main() {
-	// https://www.reactphysics3d.com/usermanual.html
+struct EventListener : rp3d::EventListener {
+	virtual void newContact(const rp3d::CollisionCallback::CollisionCallbackInfo& collisionInfo) override {
+		std::cout << '\n';
+		std::cout << "\tNew contact:"
+				<< " body1: " << reinterpret_cast<size_t>(collisionInfo.body1)
+				<< " body2: " << reinterpret_cast<size_t>(collisionInfo.body2)
+				<< " shape1: " << reinterpret_cast<size_t>(collisionInfo.proxyShape1)
+				<< " shape2: " << reinterpret_cast<size_t>(collisionInfo.proxyShape2)
+				<< '\n';
 
-	reactphysics3d::WorldSettings settings;
+		for (rp3d::ContactManifoldListElement* manifold = collisionInfo.contactManifoldElements; manifold; manifold = manifold->getNext()) {
+			const rp3d::ContactManifold* contact = manifold->getContactManifold();
+
+			std::cout << "\t\tcontact count     : " << int(contact->getNbContactPoints()) << '\n';
+			for (rp3d::ContactPoint* point = contact->getContactPoints(); point; point = point->getNext()) {
+				std::cout << '\n';
+				std::cout << "\t\tnormal            : " << libv::vec3f(point->getNormal()) << '\n';
+				std::cout << "\t\tlocalPointOnShape1: " << libv::vec3f(point->getLocalPointOnShape1()) << '\n';
+				std::cout << "\t\tlocalPointOnShape2: " << libv::vec3f(point->getLocalPointOnShape2()) << '\n';
+				std::cout << "\t\tpenetrationImpulse: " << point->getPenetrationImpulse() << '\n';
+				std::cout << "\t\tisRestingContact  : " << point->getIsRestingContact() << '\n';
+				std::cout << "\t\tpenetrationDepth  : " << point->getPenetrationDepth() << '\n';
+			}
+		}
+		std::cout << '\n' << std::endl;
+	}
+};
+
+int main() {
+	// https://www.rp3d.com/usermanual.html
+
+	rp3d::WorldSettings settings;
 	settings.defaultPositionSolverNbIterations = 10;
-	settings.defaultSleepAngularVelocity = 0.001f;
-	settings.defaultSleepLinearVelocity = 0.001f;
+	settings.defaultSleepAngularVelocity = 3.0f * libv::PI<float> / 180.0f;
+	settings.defaultSleepLinearVelocity = 0.02f;
 	settings.defaultVelocitySolverNbIterations = 15;
 	settings.isSleepingEnabled = true;
 
-	reactphysics3d::Vector3 gravity(0.0, 0.0, -9.81f);
-	reactphysics3d::DynamicsWorld world(gravity, settings);
+	rp3d::Vector3 gravity(0.0, 0.0, -9.81f);
+	rp3d::DynamicsWorld world(gravity, settings);
+	EventListener listener;
+	world.setEventListener(&listener);
 
-	const float radius = 0.5f;
 	const float mass = 1.0f;
-	reactphysics3d::Vector3 position(0.0f, 0.0f, 10.0f);
-	reactphysics3d::Quaternion orientation(1, 0, 0, 0);
-	reactphysics3d::Transform transform(position, orientation);
-	reactphysics3d::SphereShape shape(radius);
-	reactphysics3d::RigidBody* body = world.createRigidBody(transform);
-	reactphysics3d::ProxyShape* proxyShape = body->addCollisionShape(&shape, reactphysics3d::Transform::identity(), mass);
+	rp3d::Vector3 position(0.0f, 0.0f, 10.0f);
+	rp3d::Quaternion orientation(0, 0, 0, 1);
+	rp3d::Transform transform(position, orientation);
+	rp3d::BoxShape shape({1, 1, 1});
+//	rp3d::SphereShape shape(0.5f);
+	rp3d::RigidBody* body = world.createRigidBody(transform);
+	rp3d::ProxyShape* proxyShape = body->addCollisionShape(&shape, rp3d::Transform::identity(), mass);
 	proxyShape->setCollisionCategoryBits(CATEGORY1 | CATEGORY2);
 	proxyShape->setCollideWithMaskBits(CATEGORY1 | CATEGORY2);
 	body->getMaterial().setBounciness(0.4f);
 	body->getMaterial().setFrictionCoefficient(0.2f);
 
-	reactphysics3d::Vector3 floor_position(0.0f, 0.0f, 0.0f);
-	reactphysics3d::Quaternion floor_orientation(1, 0, 0, 0.1f);
-	reactphysics3d::Transform floor_transform(floor_position, floor_orientation);
-	reactphysics3d::BoxShape floorShape({100, 100, 1});
-	reactphysics3d::RigidBody* floor = world.createRigidBody(floor_transform);
-	floor->setType(reactphysics3d::BodyType::STATIC);
-	reactphysics3d::ProxyShape* floorProxyShape = floor->addCollisionShape(&floorShape, reactphysics3d::Transform::identity(), 0);
+	rp3d::Vector3 floor_position(0.0f, 0.0f, 0.0f);
+	rp3d::Quaternion floor_orientation(1, 0, 0, 0.1f);
+	rp3d::Transform floor_transform(floor_position, floor_orientation);
+	rp3d::BoxShape floorShape({100, 100, 1});
+	rp3d::RigidBody* floor = world.createRigidBody(floor_transform);
+	floor->setType(rp3d::BodyType::STATIC);
+	rp3d::ProxyShape* floorProxyShape = floor->addCollisionShape(&floorShape, rp3d::Transform::identity(), 0);
 	floorProxyShape->setCollisionCategoryBits(CATEGORY1 | CATEGORY2);
 	floorProxyShape->setCollideWithMaskBits(CATEGORY1 | CATEGORY2);
 	body->getMaterial().setBounciness(0.4f);
 	body->getMaterial().setFrictionCoefficient(0.2f);
 
 	const float timeStep = 1.0f / 60.0f;
-	for (size_t i = 0; i < 60 * 5; ++i) {
+	for (size_t i = 0; i < 60 * 4; ++i) {
 		world.update(timeStep);
-		std::cout << libv::vec3f(floor->getTransform().getPosition()) << " / " << libv::vec3f(body->getTransform().getPosition()) << std::endl;
+		std::cout
+				<< "\nobject"
+				<< "\nsleep          : " << body->isSleeping()
+				<< "\nposition       : " << libv::vec3f(body->getTransform().getPosition())
+				<< "\norientation    : " << libv::vec4f(body->getTransform().getOrientation())
+				<< "\nangles         : "
+					<< libv::rad_to_deg(libv::math::toEulerAngles(body->getTransform().getOrientation()).x) << " "
+					<< libv::rad_to_deg(libv::math::toEulerAngles(body->getTransform().getOrientation()).y) << " "
+					<< libv::rad_to_deg(libv::math::toEulerAngles(body->getTransform().getOrientation()).z)
+				<< "\nlinearVelocity : " << libv::vec3f(body->getLinearVelocity())
+				<< "\nangularVelocity: " << libv::vec3f(body->getAngularVelocity()) << std::endl;
+
 	}
 
 	world.destroyRigidBody(body);
