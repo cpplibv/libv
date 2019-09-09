40fee541da3e1067b6f551d326610d15915318ee
Császár Mátyás [Vader] <vaderhun@gmail.com>
2019-09-09 13:14:59 +0200

    tmp - libv.math: Start Angle work

--- src/libv/math/angle.hpp
+++ src/libv/math/angle.hpp
@@ -67,6 +67,40 @@ struct radian {
 	}
 };
 
-// -------------------------------------------------------------------------------------------------
+namespace math { // --------------------------------------------------------------------------------
 
+template <typename Rep, typename Res>
+struct Angle {
+	Rep value;
+
+	explicit constexpr inline Angle(Rep value) noexcept :
+		value(value) { }
+
+	constexpr inline Angle(const Angle<Rep, Res>& other) noexcept :
+		value(other.value) { }
+
+	template <typename CRep, typename CRes>
+	constexpr inline Angle(const Angle<CRep, CRes>& other) noexcept :
+		value(other.value / CRes::ratio * Res::ratio) { }
+
+	// Assignment
+	// Operators
+	// OStream
+	// ToDeg
+	// ToRad
+};
+
+template <typename Rep>
+struct ResDegrees { static constexpr Rep ratio = libv::DEG<Rep>; };
+
+template <typename Rep>
+struct ResRadian { static constexpr Rep ratio = {1}; };
+
+template <typename Rep>
+using Degrees = Angle<Rep, ResDegrees<Rep>>;
+
+template <typename Rep>
+using Radian = Angle<Rep, ResRadian<Rep>>;
+
+} // namespace math --------------------------------------------------------------------------------
 } // namespace libv
