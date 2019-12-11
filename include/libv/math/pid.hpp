4110cd62319df86b4902a164751812337d2bc299
Császár Mátyás [Vader] <vaderhun@gmail.com>
2019-12-11 14:15:02 +0100

    libv.math: Generalize PID controller to vec types

--- include/libv/math/pid.hpp
+++ include/libv/math/pid.hpp
@@ -7,26 +7,27 @@ namespace libv {
 
 // -------------------------------------------------------------------------------------------------
 
-template <typename T = double>
+template <typename T = double, typename S = double>
 class PID {
 private:
-	T Kp;
-	T Kd;
-	T Ki;
-	T pre_error = 0.0;
-	T integral = 0.0;
+	S Kp;
+	S Kd;
+	S Ki;
+	T pre_error{};
+	T integral{};
 
 public:
 	/// Kp - proportional gain (position correction)
 	/// Kd - derivative gain (speed correction)
 	/// Ki - integral gain (offset correction)
-	constexpr inline PID(T Kp, T Kd, T Ki) noexcept :
+	constexpr inline PID(S Kp, S Kd, S Ki) noexcept :
 		Kp(Kp),
 		Kd(Kd),
 		Ki(Ki) { }
 
 	/// Returns the feedback value given the delta_time, current_value and target_value
-	[[nodiscard]] constexpr inline T calculate(T delta_time, T current_value, T target_value) noexcept {
+	template <typename Time>
+	[[nodiscard]] constexpr inline T calculate(Time delta_time, T current_value, T target_value) noexcept {
 		const auto error = target_value - current_value;
 		integral += error * delta_time;
 		const auto derivative = (error - pre_error) / delta_time;
