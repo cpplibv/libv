26583758d6d4752ed61fbbe6c10be9777e152ed9
Császár Mátyás [Vader] <vaderhun@gmail.com>
2021-01-28 14:53:29 +0100

    ext: Adopt function2 for unique_function implementation

--- src/libv/utility/unique_function.hpp
+++ src/libv/utility/unique_function.hpp
@@ -2,26 +2,16 @@
 
 #pragma once
 
-// std
-#include <functional>
+// ext
+#include <function2/function2.hpp>
 
 
 namespace libv {
 
 // -------------------------------------------------------------------------------------------------
 
-// TODO P4: unique_function is a forward compatible placeholder, acquire a decent implementation
-
 template <typename Signature>
-struct unique_function : std::function<Signature> {
-	unique_function() = default;
-	unique_function(const unique_function&) = delete;
-	unique_function(unique_function&&) noexcept = default;
-	unique_function& operator=(const unique_function&) & = delete;
-	unique_function& operator=(unique_function&&) & noexcept = default;
-
-	using std::function<Signature>::function;
-};
+using unique_function = fu2::unique_function<Signature>;
 
 // -------------------------------------------------------------------------------------------------
 
