ad3b384baed89fdb222dae2e778d6b5a06c90730
Császár Mátyás [Vader] <vaderhun@gmail.com>
2021-01-30 20:50:52 +0100

    libv.math: Adopt distance related utility function from iris

---
+++ src/libv/math/distance/distance.hpp
@@ -0,0 +1,55 @@
+// Project: libv.math, File: src/libv/math/distance/distance.hpp, Author: Császár Mátyás [Vader]
+
+#pragma once
+
+// libv
+#include <libv/math/vec.hpp>
+#include <libv/math/solve_quadratic.hpp>
+// std
+#include <cmath>
+
+
+namespace iris {
+
+// -------------------------------------------------------------------------------------------------
+
+inline auto distanceTestLineToSphere(libv::vec3f linePos, libv::vec3f lineDir, libv::vec3f spherePos, float sphereRadius) {
+	auto dir = lineDir;
+
+	float t0, t1;
+
+	auto l = linePos - spherePos;
+	auto a = dir.lengthSQ();
+	auto b = 2 * libv::vec::dot(dir, l);
+	auto c = l.lengthSQ() - sphereRadius * sphereRadius;
+
+	if (!solve_quadratic(a, b, c, t0, t1))
+		return std::make_pair(false, 0.f);
+
+	if (t0 > t1)
+		std::swap(t0, t1);
+
+	if (t0 < 0.f) {
+		t0 = t1;
+		if (t0 < 0.f)
+			return std::make_pair(false, 0.f);
+	}
+
+	return std::make_pair(true, t0);
+}
+
+// http://mathworld.wolfram.com/Point-LineDistance3-Dimensional.html
+inline auto distanceLineToPoint(libv::vec3f linePos, libv::vec3f lineDir, libv::vec3f point) {
+	auto w = point - linePos;
+
+	auto c1 = libv::vec::dot(w, lineDir);
+	auto c2 = lineDir.lengthSQ();
+	auto b = c1 / c2;
+
+	auto pb = linePos + b * lineDir;
+	return (point - pb).length();
+}
+
+// -------------------------------------------------------------------------------------------------
+
+} // namespace iris
