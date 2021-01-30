cc7583e8de11aa2d0105459f2b23b28d1721cfa4
Császár Mátyás [Vader] <vaderhun@gmail.com>
2021-01-30 20:51:15 +0100

    libv.math: Adopt solve_quadratic function from iris

---
+++ src/libv/math/solve_quadratic.hpp
@@ -0,0 +1,39 @@
+// Project: libv.math, File: src/libv/math/solve_quadratic.hpp, Author: Császár Mátyás [Vader]
+
+#pragma once
+
+// libv
+#include <libv/math/vec.hpp>
+#include <libv/utility/float_equal.hpp>
+// std
+#include <cmath>
+
+
+namespace libv {
+
+// -------------------------------------------------------------------------------------------------
+
+bool solve_quadratic(float a, float b, float c, float& x0, float& x1) {
+	float discr = b * b - 4 * a * c;
+
+	if (discr < 0) {
+		return false;
+
+	} else if (libv::float_equal(discr, 0)) {
+		x0 = x1 = -0.5f * b / a;
+
+	} else {
+		float q = -0.5f * (b + std::copysign(std::sqrt(discr), b));
+		x0 = q / a;
+		x1 = c / q;
+	}
+
+	if (x0 > x1)
+		std::swap(x0, x1);
+
+	return true;
+}
+
+// -------------------------------------------------------------------------------------------------
+
+} // namespace libv
