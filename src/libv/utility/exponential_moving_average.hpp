fc83f53d9670b778d5e11d631871b80473483a54
Császár Mátyás [Vader] <vaderhun@gmail.com>
2021-03-23 01:22:01 +0100

    libv.utility: Implement exponential_moving_average

---
+++ src/libv/utility/exponential_moving_average.hpp
@@ -0,0 +1,48 @@
+// Created by Vader on 2021.03.22..
+
+#pragma once
+
+// std
+#include <cmath>
+
+
+namespace libv {
+
+// -------------------------------------------------------------------------------------------------
+
+template <typename T, int Window = 10>
+// TODO P5: GCC 11, floating point types as template arguments
+//template <typename T, T Window = T{10}, T Match = T{0.9}>
+struct exponential_moving_average {
+public:
+//	static constexpr T match = Match;
+	static constexpr T match = 0.9;
+	static constexpr T window = Window;
+	static constexpr T alpha = -std::pow(T{1} - match, T{1} / window) + T{1};
+
+private:
+	T accumulator;
+
+public:
+	explicit constexpr inline exponential_moving_average(T init = T{}) noexcept :
+		accumulator(init) {}
+
+public:
+	constexpr inline void add(T value) noexcept {
+		accumulator = alpha * value + (T{1} - alpha) * accumulator;
+	}
+
+	constexpr inline void add(T value, T weight) noexcept {
+		const auto weighted_alpha = T{1} - std::pow(T{1} - alpha, weight);
+		accumulator = weighted_alpha * value + (T{1} - weighted_alpha) * accumulator;
+	}
+
+	[[nodiscard]] constexpr inline T value() const noexcept {
+		return accumulator;
+	}
+};
+
+// -------------------------------------------------------------------------------------------------
+
+} // namespace libv
+
---
+++ test/libv/utility/test_exponential_moving_average.cpp
@@ -0,0 +1,209 @@
+// Project: libv.utility, File: test/libv/utility/test_extract_bits.cpp, Author: Császár Mátyás [Vader]
+
+// hpp
+#include <catch/catch.hpp>
+// pro
+#include <libv/utility/approx.hpp>
+#include <libv/utility/exponential_moving_average.hpp>
+
+
+//#include <random>
+//#include <iostream>
+//#include <libv/utility/uniform_distribution.hpp>
+
+// -------------------------------------------------------------------------------------------------
+
+TEST_CASE("exponential_moving_average", "[libv.utility.exponential_moving_average]") {
+
+	libv::exponential_moving_average<double, 10> avg01{0};
+	libv::exponential_moving_average<double, 10> avg02{0};
+	libv::exponential_moving_average<double, 10> avg05{0};
+	libv::exponential_moving_average<double, 10> avg10{0};
+	libv::exponential_moving_average<double, 10> avg20{0};
+	libv::exponential_moving_average<double, 10> avg50{0};
+//	libv::exponential_moving_average<double, 10.0, 0.9> avg01{0};
+//	libv::exponential_moving_average<double, 10.0, 0.9> avg02{0};
+//	libv::exponential_moving_average<double, 10.0, 0.9> avg05{0};
+//	libv::exponential_moving_average<double, 10.0, 0.9> avg10{0};
+//	libv::exponential_moving_average<double, 10.0, 0.9> avg20{0};
+//	libv::exponential_moving_average<double, 10.0, 0.9> avg50{0};
+
+	CHECK(avg01.value() == libv::approx(0.0));
+	CHECK(avg02.value() == libv::approx(0.0));
+	CHECK(avg05.value() == libv::approx(0.0));
+	CHECK(avg10.value() == libv::approx(0.0));
+	CHECK(avg20.value() == libv::approx(0.0));
+	CHECK(avg50.value() == libv::approx(0.0));
+
+	for (int i = 0; i < 100; ++i) 	avg01.add(1.0, 0.1);
+	for (int i = 0; i < 50; ++i) 	avg02.add(1.0, 0.2);
+	for (int i = 0; i < 20; ++i) 	avg05.add(1.0, 0.5);
+	for (int i = 0; i < 10; ++i) 	avg10.add(1.0, 1.0);
+	for (int i = 0; i < 5; ++i) 	avg20.add(1.0, 2.0);
+	for (int i = 0; i < 2; ++i) 	avg50.add(1.0, 5.0);
+
+	CHECK(avg01.value() == libv::approx(0.9));
+	CHECK(avg02.value() == libv::approx(0.9));
+	CHECK(avg05.value() == libv::approx(0.9));
+	CHECK(avg10.value() == libv::approx(0.9));
+	CHECK(avg20.value() == libv::approx(0.9));
+	CHECK(avg50.value() == libv::approx(0.9));
+
+	for (int i = 0; i < 100; ++i) 	avg01.add(1.0, 0.1);
+	for (int i = 0; i < 50; ++i) 	avg02.add(1.0, 0.2);
+	for (int i = 0; i < 20; ++i) 	avg05.add(1.0, 0.5);
+	for (int i = 0; i < 10; ++i) 	avg10.add(1.0, 1.0);
+	for (int i = 0; i < 5; ++i) 	avg20.add(1.0, 2.0);
+	for (int i = 0; i < 2; ++i) 	avg50.add(1.0, 5.0);
+
+	CHECK(avg01.value() == libv::approx(0.99));
+	CHECK(avg02.value() == libv::approx(0.99));
+	CHECK(avg05.value() == libv::approx(0.99));
+	CHECK(avg10.value() == libv::approx(0.99));
+	CHECK(avg20.value() == libv::approx(0.99));
+	CHECK(avg50.value() == libv::approx(0.99));
+
+	// --- Test data set for random signal -------------------------------------------------------------
+
+//	libv::exponential_moving_average<double, 10> avg01;
+//	libv::exponential_moving_average<double, 10> avg02;
+//	libv::exponential_moving_average<double, 10> avg05;
+//	libv::exponential_moving_average<double, 10> avg10;
+//	libv::exponential_moving_average<double, 10> avg20;
+//	libv::exponential_moving_average<double, 10> avg50;
+//	std::mt19937_64 rd(0x5EED);
+//
+//	for (int i = 0; i < 100; ++i) {
+//		const auto b = libv::make_uniform_distribution_inclusive(10, 90)(rd);
+//		const auto r = libv::make_uniform_distribution_inclusive(0, 25)(rd);
+//		for (int j = 0; j < r; ++j) {
+//			const auto v = b + libv::make_uniform_distribution_inclusive(-10, 10)(rd);
+//			avg01.add(v, 0.1);
+//			avg02.add(v, 0.2);
+//			avg05.add(v, 0.5);
+//			avg10.add(v, 1.0);
+//			avg20.add(v, 2.0);
+//			avg50.add(v, 5.0);
+//			std::cout << v
+//					<< "\t" << avg01.value()
+//					<< "\t" << avg02.value()
+//					<< "\t" << avg05.value()
+//					<< "\t" << avg10.value()
+//					<< "\t" << avg20.value()
+//					<< "\t" << avg50.value()
+//					<< std::endl;
+//		}
+//	}
+
+	// --- Test data set for square signal -------------------------------------------------------------
+
+//	for (int i = 0; i < 100; ++i) {
+//		const auto v = 1;
+//		avg01.add(v, 0.1);
+//		avg02.add(v, 0.2);
+//		avg05.add(v, 0.5);
+//		avg10.add(v, 1.0);
+//		avg20.add(v, 2.0);
+//		avg50.add(v, 5.0);
+//		std::cout << v
+//				<< "\t" << avg01.value()
+//				<< "\t" << avg02.value()
+//				<< "\t" << avg05.value()
+//				<< "\t" << avg10.value()
+//				<< "\t" << avg20.value()
+//				<< "\t" << avg50.value()
+//				<< std::endl;
+//	}
+//
+//	for (int i = 0; i < 100; ++i) {
+//		const auto v = 0;
+//		avg01.add(v, 0.1);
+//		avg02.add(v, 0.2);
+//		avg05.add(v, 0.5);
+//		avg10.add(v, 1.0);
+//		avg20.add(v, 2.0);
+//		avg50.add(v, 5.0);
+//		std::cout << v
+//				<< "\t" << avg01.value()
+//				<< "\t" << avg02.value()
+//				<< "\t" << avg05.value()
+//				<< "\t" << avg10.value()
+//				<< "\t" << avg20.value()
+//				<< "\t" << avg50.value()
+//				<< std::endl;
+//	}
+//
+//	for (int i = 0; i < 100; ++i) {
+//		const auto v = 1;
+//		avg01.add(v, 0.1);
+//		avg02.add(v, 0.2);
+//		avg05.add(v, 0.5);
+//		avg10.add(v, 1.0);
+//		avg20.add(v, 2.0);
+//		avg50.add(v, 5.0);
+//		std::cout << v
+//				<< "\t" << avg01.value()
+//				<< "\t" << avg02.value()
+//				<< "\t" << avg05.value()
+//				<< "\t" << avg10.value()
+//				<< "\t" << avg20.value()
+//				<< "\t" << avg50.value()
+//				<< std::endl;
+//	}
+//
+//	for (int i = 0; i < 100; ++i) {
+//		const auto v = 3;
+//		avg01.add(v, 0.1);
+//		avg02.add(v, 0.2);
+//		avg05.add(v, 0.5);
+//		avg10.add(v, 1.0);
+//		avg20.add(v, 2.0);
+//		avg50.add(v, 5.0);
+//		std::cout << v
+//				<< "\t" << avg01.value()
+//				<< "\t" << avg02.value()
+//				<< "\t" << avg05.value()
+//				<< "\t" << avg10.value()
+//				<< "\t" << avg20.value()
+//				<< "\t" << avg50.value()
+//				<< std::endl;
+//	}
+//
+//	for (int i = 0; i < 100; ++i) {
+//		const auto v = 4;
+//		avg01.add(v, 0.1);
+//		avg02.add(v, 0.2);
+//		avg05.add(v, 0.5);
+//		avg10.add(v, 1.0);
+//		avg20.add(v, 2.0);
+//		avg50.add(v, 5.0);
+//		std::cout << v
+//				<< "\t" << avg01.value()
+//				<< "\t" << avg02.value()
+//				<< "\t" << avg05.value()
+//				<< "\t" << avg10.value()
+//				<< "\t" << avg20.value()
+//				<< "\t" << avg50.value()
+//				<< std::endl;
+//	}
+//
+//	for (int i = 0; i < 100; ++i) {
+//		const auto v = 1;
+//		avg01.add(v, 0.1);
+//		avg02.add(v, 0.2);
+//		avg05.add(v, 0.5);
+//		avg10.add(v, 1.0);
+//		avg20.add(v, 2.0);
+//		avg50.add(v, 5.0);
+//		std::cout << v
+//				<< "\t" << avg01.value()
+//				<< "\t" << avg02.value()
+//				<< "\t" << avg05.value()
+//				<< "\t" << avg10.value()
+//				<< "\t" << avg20.value()
+//				<< "\t" << avg50.value()
+//				<< std::endl;
+//	}
+}
+
+// -------------------------------------------------------------------------------------------------
