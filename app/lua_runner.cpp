c83d250ba224462102e4093997e344ad58360204
Császár Mátyás [Vader] <vaderhun@gmail.com>
2021-03-27 01:36:10 +0100

    app.lua_runner: Adopt lua runner app from iris

---
+++ app/lua_runner.cpp
@@ -0,0 +1,27 @@
+// Project: iris, File: sandbox/lua_runner.cpp, Author: Császár Mátyás [Vader]
+
+// std
+#include <iostream>
+// pro
+#include <libv/lua/lua.hpp>
+
+
+// -------------------------------------------------------------------------------------------------
+
+int main(int argc, const char* argv[]) {
+	if (argc < 2) {
+		std::cerr << "usage: " << argv[0] << " <lua_file>" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	try {
+		auto lua = libv::lua::create_state(libv::lua::lualib::base | libv::lua::lualib::vec);
+		lua["package"]["path"] = ";script/?.lua";
+		lua.script_file(argv[1]);
+		return EXIT_SUCCESS;
+
+	} catch (const sol::error& e) {
+		std::cout << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+}
