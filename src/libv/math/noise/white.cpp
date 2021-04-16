d96f208e24b2f1b1f92abecf051002c7c553cdb0
Császár Mátyás [Vader] <vaderhun@gmail.com>
2021-04-16 02:20:11 +0200

    libv.math: Implement white noise

---
+++ src/libv/math/noise/white.cpp
@@ -0,0 +1,47 @@
+// Project: libv.math, File: src/libv/math/noise/white.cpp, Author: Császár Mátyás [Vader]
+
+// hpp
+#include <libv/math/noise/white.hpp>
+// libv
+#include <libv/utility/uniform_distribution.hpp>
+// std
+#include <random>
+
+
+namespace libv {
+
+// -------------------------------------------------------------------------------------------------
+
+//std::vector<double> noise_white_2D(uint64_t seed, uint32_t X, uint32_t Y) {
+//	std::mt19937_64 rng(seed);
+//	auto dist = libv::make_uniform_distribution_inclusive(0.0, 1.0);
+//
+//	std::vector<double> result;
+//	result.resize(X * Y);
+//
+//	for (uint32_t y = 0; y < Y; y++)
+//		for (uint32_t x = 0; x < X; x++)
+//			result[y * Y + x] = dist(rng);
+//
+//	return result;
+//}
+
+std::vector<libv::vec3uc> noise_white_2D_3uc(uint64_t seed, uint32_t X, uint32_t Y) {
+	std::mt19937_64 rng(seed);
+	auto dist = libv::make_uniform_distribution_inclusive(uint8_t(0), uint8_t(255));
+
+	std::vector<libv::vec3uc> result;
+	result.resize(X * Y);
+
+	for (uint32_t i = 0; i < X * Y; i++) {
+		result[i].x = dist(rng);
+		result[i].y = dist(rng);
+		result[i].z = dist(rng);
+	}
+
+	return result;
+}
+
+// -------------------------------------------------------------------------------------------------
+
+} // namespace libv
---
+++ src/libv/math/noise/white.hpp
@@ -0,0 +1,21 @@
+// Project: libv.math, File: src/libv/math/noise/white.hpp, Author: Császár Mátyás [Vader]
+
+#pragma once
+
+// libv
+#include <libv/math/vec.hpp>
+// std
+#include <cstdint>
+#include <vector>
+
+
+namespace libv {
+
+// -------------------------------------------------------------------------------------------------
+
+//std::vector<double> noise_white_2D(uint64_t seed, uint32_t X, uint32_t Y);
+std::vector<libv::vec3uc> noise_white_2D_3uc(uint64_t seed, uint32_t X, uint32_t Y);
+
+// -------------------------------------------------------------------------------------------------
+
+} // namespace libv
