2d873dc51dde22b097710175b9b185c4dd42b1ea
Császár Mátyás [Vader] <vaderhun@gmail.com>
2021-07-03 02:24:36 +0200

    libv.ui: Reimplement layout testing setup and revive panel_line tests

--- test/libv/ui/test_layout_utility.hpp
+++ test/libv/ui/test_layout_utility.hpp
@@ -6,128 +6,155 @@
 #include <libv/math/vec.hpp>
 #include <libv/utility/approx.hpp>
 // std
-#include <memory>
 #include <ostream>
 // pro
-//#include <libv/ui/base_component.hpp>
+//#include <libv/ui/context/context_style.hpp>
+//#include <libv/ui/style.hpp>
+#include <libv/ui/component/detail/component_api.hpp>
+#include <libv/ui/component/detail/core_component.hpp>
+#include <libv/ui/component/detail/core_ptr.hpp>
 #include <libv/ui/context/context_layout.hpp>
-#include <libv/ui/context/context_style.hpp>
+#include <libv/ui/context/context_ui_link.hpp>
 #include <libv/ui/parse/parse_size.hpp>
-#include <libv/ui/style.hpp>
+#include <libv/utility/approx.hpp>
 
-
-// TODO P5: Layout separation was removed, a ui component based testing is required, re-enable these tests
 
 // -------------------------------------------------------------------------------------------------
 
-//namespace {
-//
-//using Pos = libv::vec3_t<libv::Approx<float>>;
-//using Size = libv::vec3_t<libv::Approx<float>>;
-//
-//struct Bounds {
-//	libv::vec3f position;
-//	libv::vec3f size;
-//
-//	Bounds(libv::vec3f position, libv::vec3f size) :
-//		position(position),
-//		size(size) { }
-//
-//	Bounds(float px, float py, float sx, float sy) :
-//		position(px, py, 0),
-//		size(sx, sy, 0) { }
-//
-//	Bounds(float px, float py, float pz, float sx, float sy, float sz) :
-//		position(px, py, pz),
-//		size(sx, sy, sz) { }
-//
-//	bool friend operator==(const Bounds& lhs, const Bounds& rhs) {
-//		const auto rhs_pos_approx = libv::vec3_t<libv::Approx<float>>{rhs.position};
-//		const auto rhs_size_approx = libv::vec3_t<libv::Approx<float>>{rhs.size};
-//		return lhs.position == rhs_pos_approx && lhs.size == rhs_size_approx;
-//	}
-//
-//	friend std::ostream& operator<<(std::ostream& os, const Bounds& bounds) {
-//		return os << "p:" << bounds.position << ", s:" << bounds.size;
-//	}
-//};
-//
-//class TestComponent : public libv::ui::CoreComponent {
-//public:
-//	libv::vec3f dynamicSize = {};
-//
-//	TestComponent(CoreComponent& parent) :
-//			libv::ui::CoreComponent(parent, libv::ui::GenerateName, "test") {};
-//
-//	virtual libv::vec3f doLayout1(const libv::ui::ContextLayout1&) override {
-//		return dynamicSize;
-//	}
-//};
-//
-//template <typename T>
-//class BasicTestChild {
-//protected:
-//	std::vector<typename T::Child>& components;
-//	size_t index;
-//
-//public:
-//	BasicTestChild(std::vector<typename T::Child>& components, size_t index) :
-//		components(components), index(index) { }
-//
-//public:
-//	void dynamic(libv::vec3f value) {
-//		static_cast<TestComponent&>(components[index].ptr.core()).dynamicSize = value;
-//	}
-//
-//	void dynamic(float x, float y, float z) {
-//		dynamic({x, y, z});
-//	}
-//
-//	void dynamic(float x, float y) {
-//		dynamic(x, y, 0);
-//	}
-//
-//	Bounds bounds() {
-//		return {components[index].ptr.core().position(), components[index].ptr.core().size()};
-//	}
-//};
-//
-//template <typename T, typename TC>
-//class BasicTestLayout {
-//protected:
-//	TestComponent ignore{static_cast<libv::ui::CoreComponent&>(ignore)};
-//	// Fix UB: passing an uninitialized object address to itself to copy the context ptr that is not used
-//	typename T::Properties property;
-//	std::vector<typename T::Child> components;
-//
-//public:
-//	BasicTestLayout() {
-//		// Initialize property with fallback values
-//		libv::ui::ContextStyle ctx{nullptr, ignore};
-//		T::style(property, ctx);
-//	}
-//
-//public:
-//	TC add() {
-//		auto sp = std::make_shared<TestComponent>(static_cast<libv::ui::CoreComponent&>(ignore));
-//		auto& child = components.emplace_back(sp);
-//
-//		// Initialize child property with fallback values
-//		libv::ui::ContextStyle ctx{nullptr, ignore};
-//		T::style(child.property, ctx);
-//
-//		return TC{components, components.size() - 1};
-//	}
-//
-//	libv::vec3f layout1(libv::ui::ContextLayout1& le) {
-//		return T::layout1(le, components, property, ignore);
-//	}
-//
-//	void layout2(libv::ui::ContextLayout2& le) {
-//		return T::layout2(le, components, property, ignore);
-//	}
-//};
-//
-//} // namespace
+using approx_size = libv::vec3_t<libv::approx<float, -3>>;
+using approx_pos = libv::vec3_t<libv::approx<float, -3>>;
+
+struct Bounds {
+	libv::vec3f position;
+	libv::vec3f size;
+
+	Bounds(libv::vec3f position, libv::vec3f size) :
+			position(position),
+			size(size) { }
+
+	Bounds(float px, float py, float sx, float sy) :
+			position(px, py, 0),
+			size(sx, sy, 0) { }
+
+	Bounds(float px, float py, float pz, float sx, float sy, float sz) :
+			position(px, py, pz),
+			size(sx, sy, sz) { }
+
+	bool friend operator==(const Bounds& lhs, const Bounds& rhs) {
+		const auto rhs_pos_approx = libv::vec3_t<libv::approx<float>>{rhs.position};
+		const auto rhs_size_approx = libv::vec3_t<libv::approx<float>>{rhs.size};
+		return lhs.position == rhs_pos_approx && lhs.size == rhs_size_approx;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const Bounds& bounds) {
+		return os << "p:" << bounds.position << ", s:" << bounds.size;
+	}
+};
+
+struct DynamicTag : libv::vec3f{
+	using libv::vec3f::vec3f;
+};
+struct MarginTag : libv::vec4f{
+	using libv::vec4f::vec4f;
+};
+struct PaddingTag : libv::vec4f{
+	using libv::vec4f::vec4f;
+};
+
+using D = DynamicTag;
+using M = MarginTag;
+using P = PaddingTag;
+
+struct CoreTestComponent : libv::ui::CoreComponent {
+	libv::vec3f injected_dynamic_size;
+
+	using CoreComponent::CoreComponent;
+
+protected:
+	virtual libv::vec3f doLayout1(const libv::ui::ContextLayout1& environment) override {
+		(void) environment;
+		return injected_dynamic_size + padding_size3();
+	}
+};
+
+struct TestComponent : libv::ui::ComponentAPI<libv::ui::Component, TestComponent, class CoreTestComponent, libv::ui::EventHostGeneral> {
+public:
+	using libv::ui::ComponentAPI<libv::ui::Component, TestComponent, class CoreTestComponent, libv::ui::EventHostGeneral>::ComponentAPI;
+	static constexpr std::string_view component_type = "test";
+	static libv::ui::core_ptr create_core(std::string name) {
+		return libv::ui::create_core_ptr<CoreTestComponent>(std::move(name));
+	}
+
+	void injected_dynamic_size(libv::vec3f v) {
+		self().injected_dynamic_size = v;
+	}
+
+	Bounds bounds() const {
+		return {layout_position(), layout_size()};
+	}
+};
+
+template <typename T>
+struct TestPanel : T {
+	TestPanel() : T((libv::ui::current_thread_context(reinterpret_cast<libv::ui::ContextUI&>(*this)), "layout")) {
+		// Hacky injection of this as a non-null contextUI, it will not be used anyways
+
+		// Hacky styling to initialize properties of only this panel (Child are not yet attached, their non-base properties are ignored during testing)! AND It will crash if ContextUI is used to init a property
+		libv::ui::AccessRoot::styleScan(this->core());
+	}
+
+private:
+	void apply(TestComponent& comp) {
+		(void) comp;
+	};
+
+	template <typename... Tags>
+	void apply(TestComponent& comp, DynamicTag t, Tags&&... tags) {
+		comp.injected_dynamic_size(t);
+		apply(comp, std::forward<Tags>(tags)...);
+	}
+
+	template <typename... Tags>
+	void apply(TestComponent& comp, PaddingTag t, Tags&&... tags) {
+		comp.padding(t);
+		apply(comp, std::forward<Tags>(tags)...);
+	}
+
+	template <typename... Tags>
+	void apply(TestComponent& comp, MarginTag t, Tags&&... tags) {
+		comp.padding(t);
+		apply(comp, std::forward<Tags>(tags)...);
+	}
+
+public:
+	template <typename... Tags>
+	TestComponent add(const std::string_view size, Tags&&... tags) {
+		TestComponent comp;
+		comp.size(libv::ui::parse_size_or_throw(size));
+		apply(comp, std::forward<Tags>(tags)...);
+		T::add(comp);
+		return comp;
+	}
+
+public:
+	libv::vec3f layout1(libv::vec3f constraint_size) {
+		return libv::ui::AccessRoot::layout1(this->core(), libv::ui::ContextLayout1{constraint_size});
+	}
+	libv::vec3f layout1(float cx, float cy, float cz = 0.f) {
+		return layout1({cx, cy, cz});
+	}
+	void layout2(libv::vec3f pos, libv::vec3f size) {
+		libv::ui::AccessRoot::layout2(this->core(), libv::ui::ContextLayout2{pos, size});
+	}
+	void layout2(float px, float py, float pz, float sx, float sy, float sz) {
+		layout2({px, py, pz}, {sx, sy, sz});
+	}
+	void layout2(float px, float py, float sx, float sy) {
+		layout2({px, py, 0}, {sx, sy, 0});
+	}
+	void layout2(float sx, float sy, float sz = 0) {
+		layout2({0, 0, 0}, {sx, sy, sz});
+	}
+};
 
 // -------------------------------------------------------------------------------------------------
