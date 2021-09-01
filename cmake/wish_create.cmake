7b688cffe3e129ef979621f7b4074922b0ab62fb
Császár Mátyás [Vader] <vaderhun@gmail.com>
2021-09-01 10:44:34 +0200

    wish: Add support for source generators

--- cmake/wish_create.cmake
+++ cmake/wish_create.cmake
@@ -4,45 +4,35 @@
 # --- Options --------------------------------------------------------------------------------------
 
 set(__wish_configure_externals 0)
-
 macro(wish_enable_configure_externals)
 	set(__wish_configure_externals 1)
 endmacro()
-
 macro(wish_disable_configure_externals)
 	set(__wish_configure_externals 0)
 endmacro()
 
-# --- Group ----------------------------------------------------------------------------------------
-
 set(__wish_global_debug 0)
-
 macro(wish_enable_debug)
 	set(__wish_global_debug 1)
 endmacro()
-
 macro(wish_disable_debug)
 	set(__wish_global_debug 0)
 endmacro()
-
-# --- Group ----------------------------------------------------------------------------------------
-
-set(__wish_current_group_stack)
-
-macro(wish_create_group arg_GROUP)
-	list(APPEND __wish_current_group_stack ${arg_GROUP})
-endmacro()
-
-macro(wish_end_group)
-	list(REMOVE_AT __wish_current_group_stack -1)
-endmacro()
-
-# --- Group ----------------------------------------------------------------------------------------
 
 set(__wish_static_link_std)
 
 macro(wish_static_link_std)
 	set(__wish_static_link_std -static)
+endmacro()
+
+# --- Group ----------------------------------------------------------------------------------------
+
+set(__wish_current_group_stack)
+macro(wish_create_group arg_GROUP)
+	list(APPEND __wish_current_group_stack ${arg_GROUP})
+endmacro()
+macro(wish_end_group)
+	list(REMOVE_AT __wish_current_group_stack -1)
 endmacro()
 
 # --- IDE ------------------------------------------------------------------------------------------
@@ -55,9 +45,14 @@ function(wish_create_ide_target)
 	# TODO P5: list targets / libraries / executables
 	add_custom_target(wish_ide
 		COMMAND ${CMAKE_COMMAND} -E echo "External include directories:"
-		COMMAND ${CMAKE_COMMAND} -E echo "'${__wish_external_include_directories}'"
+		COMMAND ${CMAKE_COMMAND} -E echo "${__wish_external_include_directories}"
 		COMMAND ${CMAKE_COMMAND} -E echo "External defines:"
-		COMMAND ${CMAKE_COMMAND} -E echo "'${__wish_external_defines}'"
+		COMMAND ${CMAKE_COMMAND} -E echo "${__wish_external_defines}"
+		VERBATIM
+	)
+
+	add_custom_target(wish
+		COMMAND ${CMAKE_COMMAND} -E echo Wish
 	)
 endfunction()
 
@@ -142,10 +137,73 @@ function(wish_create_external)
 	endif()
 endfunction()
 
+# --- Generate ------------------------------------------------------------------------------------
+
+set(__wish_generators "")
+# __wish_generator_command_${generator_name} stores the generator commands
+# __wish_generator_output_rules_${generator_name} stores the generator input -> outputs rewrite rules
+
+function(wish_generator)
+	cmake_parse_arguments(arg "" "TARGET;COMMAND" "" ${ARGN})
+
+	set(temp_list ${__wish_generators})
+	list(APPEND temp_list ${arg_TARGET})
+	set(__wish_generators ${temp_list} PARENT_SCOPE)
+
+	set(__wish_generator_command_${arg_TARGET} ${arg_COMMAND} PARENT_SCOPE)
+
+	# Have to hand parse outputs, as list of list as argument is not yet supported...
+	set(output_rules ${arg_UNPARSED_ARGUMENTS})
+	list(POP_FRONT output_rules)
+	set(__wish_generator_output_rules_${arg_TARGET} ${output_rules} PARENT_SCOPE)
+endfunction()
+
+function(wish_generate out_generated_outputs)
+	cmake_parse_arguments(arg "" "" "${__wish_generators}" ${ARGN})
+
+	set(generated_outputs "")
+
+	foreach(generator ${__wish_generators})
+		file(GLOB_RECURSE matching_files RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} CONFIGURE_DEPENDS ${arg_${generator}})
+
+		foreach(matching_file ${matching_files})
+			set(output_files "")
+			set(output_rules_left ${__wish_generator_output_rules_${generator}})
+			while(output_rules_left)
+				list(FIND output_rules_left "OUTPUT" end_index)
+
+				list(SUBLIST output_rules_left 0 ${end_index} output_rule)
+				# Use ${output_rule} list
+				string(${output_rule} output_file ${matching_file})
+				list(APPEND output_files ${output_file})
+
+				# Jump to next segment
+				if (${end_index} EQUAL -1)
+					set(output_rules_left "")
+				else()
+					MATH(EXPR end_index_p_1 "${end_index}+1")
+					list(SUBLIST output_rules_left ${end_index_p_1} -1 output_rules_left)
+				endif()
+			endwhile()
+
+			add_custom_command(
+					OUTPUT  ${output_files}
+					COMMAND ${__wish_generator_command_${generator}} ${matching_file} ${output_files}
+					DEPENDS ${generator} ${matching_file}
+					WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+			)
+
+			list(APPEND generated_outputs ${output_files})
+		endforeach()
+	endforeach()
+
+	set(${out_generated_outputs} ${generated_outputs} PARENT_SCOPE)
+endfunction()
+
 # --- Executable -----------------------------------------------------------------------------------
 
 function(wish_create_executable)
-	cmake_parse_arguments(arg "DEBUG" "TARGET" "SOURCE;OBJECT;LINK" ${ARGN})
+	cmake_parse_arguments(arg "DEBUG" "TARGET" "SOURCE;OBJECT;GENERATE;LINK" ${ARGN})
 
 	# check
 	if(NOT arg_SOURCE AND NOT arg_OBJECT)
@@ -158,8 +216,16 @@ function(wish_create_executable)
 		list(APPEND matching_sources $<TARGET_OBJECTS:${obj}>)
 	endforeach()
 
+	# generated files
+	if(arg_GENERATE)
+		wish_generate(generated_outputs ${arg_GENERATE})
+	endif()
+
 	# add
-	add_executable(${arg_TARGET} ${matching_sources})
+	set(every_source ${matching_sources} ${generated_outputs})
+	list(REMOVE_DUPLICATES every_source)
+
+	add_executable(${arg_TARGET} ${every_source})
 	target_link_libraries(${arg_TARGET} ${arg_LINK} ${__wish_static_link_std})
 	target_link_libraries(${arg_TARGET} ${obj})
 
@@ -184,7 +250,7 @@ endfunction()
 # --- Library --------------------------------------------------------------------------------------
 
 function(wish_create_library)
-	cmake_parse_arguments(arg "DEBUG;STATIC;SHARED;INTERFACE" "TARGET" "SOURCE;OBJECT;LINK" ${ARGN})
+	cmake_parse_arguments(arg "DEBUG;STATIC;SHARED;INTERFACE" "TARGET" "SOURCE;OBJECT;GENERATE;LINK" ${ARGN})
 
 	# check
 	list(GET arg_TARGET 0 arg_target_name)
@@ -201,15 +267,23 @@ function(wish_create_library)
 		list(APPEND matching_sources $<TARGET_OBJECTS:${obj}>)
 	endforeach()
 
+	# generated files
+	if(arg_GENERATE)
+		wish_generate(generated_outputs ${arg_GENERATE})
+	endif()
+
 	# add_library
+	set(every_source ${matching_sources} ${target_objects} ${generated_outputs})
+	list(REMOVE_DUPLICATES every_source)
+
 	if(arg_STATIC)
-		add_library(${arg_TARGET} STATIC ${matching_sources} ${target_objects})
+		add_library(${arg_TARGET} STATIC ${every_source})
 		target_link_libraries(${arg_TARGET} ${arg_LINK} ${__wish_static_link_std})
 	elseif(arg_SHARED)
-		add_library(${arg_TARGET} SHARED ${matching_sources} ${target_objects})
+		add_library(${arg_TARGET} SHARED ${every_source})
 		target_link_libraries(${arg_TARGET} ${arg_LINK} ${__wish_static_link_std})
 	elseif(arg_INTERFACE)
-		add_library(${arg_TARGET} INTERFACE ${matching_sources} ${target_objects})
+		add_library(${arg_TARGET} INTERFACE ${every_source})
 		target_link_libraries(${arg_TARGET} INTERFACE ${arg_LINK})
 	else()
 		message(FATAL_ERROR "Library has to be either STATIC, SHARED or INTERFACE")
@@ -231,6 +305,8 @@ function(wish_create_library)
 		message("	Glob      : ${arg_SOURCE}")
 		message("	Source    : ${matching_sources}")
 		message("	Object    : ${arg_OBJECT}")
+		message("	Generate  : ${arg_GENERATE}")
+		message("	Generated : ${generated_outputs}")
 		message("	Link      : ${arg_LINK}")
 		message("	Static    : ${arg_STATIC}")
 		message("	Shared    : ${arg_SHARED}")
@@ -242,7 +318,7 @@ endfunction()
 # --- Object ---------------------------------------------------------------------------------------
 
 function(wish_create_object)
-	cmake_parse_arguments(arg "DEBUG" "TARGET" "SOURCE;OBJECT;LINK" ${ARGN})
+	cmake_parse_arguments(arg "DEBUG" "TARGET" "SOURCE;OBJECT;GENERATE;LINK" ${ARGN})
 
 	# check
 	if(NOT arg_SOURCE AND NOT arg_OBJECT)
@@ -255,8 +331,16 @@ function(wish_create_object)
 		list(APPEND matching_sources $<TARGET_OBJECTS:${obj}>)
 	endforeach()
 
+	# generated files
+	if(arg_GENERATE)
+		wish_generate(generated_outputs ${arg_GENERATE})
+	endif()
+
 	# add
-	add_library(${arg_TARGET} OBJECT ${matching_sources})
+	set(every_source ${matching_sources} ${generated_outputs})
+	list(REMOVE_DUPLICATES every_source)
+
+	add_library(${arg_TARGET} OBJECT ${every_source})
 
 	# group
 	foreach(group ${__wish_current_group_stack})
@@ -270,6 +354,8 @@ function(wish_create_object)
 		message("Object target: ${arg_TARGET}")
 		message("	Glob      : ${arg_SOURCE}")
 		message("	Source    : ${matching_sources}")
+		message("	Generate  : ${arg_GENERATE}")
+		message("	Generated : ${generated_outputs}")
 		message("	Link      : ${arg_LINK}")
 		message("	Object    : ${arg_OBJECT}")
 		message("	Group     : ${__wish_current_group_stack}")
