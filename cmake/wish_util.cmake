c9e7ba69751e9533f1adf469f3242c60e8c1fc46
Császár Mátyás [Vader] <vaderhun@gmail.com>
2021-09-13 01:38:04 +0200

    wish: Switch to cmake's native ProcessorCount function

--- cmake/wish_util.cmake
+++ cmake/wish_util.cmake
@@ -5,11 +5,8 @@
 if(NOT DEFINED WISH_PROCESSOR_COUNT)
 	set(WISH_PROCESSOR_COUNT 1) # Unknown
 
-	if(NOT WIN32)
-		execute_process(COMMAND "nproc" OUTPUT_VARIABLE WISH_PROCESSOR_COUNT)
-	else()
-		set(WISH_PROCESSOR_COUNT "$ENV{NUMBER_OF_PROCESSORS}")
-	endif()
+	include(ProcessorCount)
+	ProcessorCount(WISH_PROCESSOR_COUNT)
 endif()
 
 # --------------------------------------------------------------------------------------------------
