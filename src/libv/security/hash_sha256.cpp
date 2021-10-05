ea99efd9f8cd1087385119ac43973e4d9e1196b9
Császár Mátyás [Vader] <vaderhun@gmail.com>
2021-10-05 06:52:40 +0200

    libv.security: Add SHA256 hash functions

---
+++ src/libv/security/hash_sha256.cpp
@@ -0,0 +1,32 @@
+// Project: libv.security, File: src/libv/security/hash_sha256.cpp, Author: Császár Mátyás [Vader]
+
+// hpp
+#include <libv/security/hash_sha256.hpp>
+// ext
+#include <openssl/sha.h>
+
+
+namespace libv {
+namespace security {
+
+// -------------------------------------------------------------------------------------------------
+
+SHA256 hash_sha256(std::span<const std::byte> data) {
+	SHA256 result;
+
+	SHA256_CTX sha256;
+	SHA256_Init(&sha256);
+	SHA256_Update(&sha256, data.data(), data.size());
+	SHA256_Final(reinterpret_cast<unsigned char*>(&result.value), &sha256);
+
+	return result;
+}
+
+SHA256 hash_sha256(std::string_view data) {
+	return hash_sha256(std::span<const std::byte>(reinterpret_cast<const std::byte*>(data.data()), data.size()));
+}
+
+// -------------------------------------------------------------------------------------------------
+
+} // namespace security
+} // namespace libv
---
+++ src/libv/security/hash_sha256.hpp
@@ -0,0 +1,25 @@
+// Project: libv.security, File: src/libv/security/hash_sha256.hpp, Author: Császár Mátyás [Vader]
+
+#pragma once
+
+// std
+#include <span>
+#include <string_view>
+
+
+namespace libv {
+namespace security {
+
+// -------------------------------------------------------------------------------------------------
+
+struct SHA256 {
+	std::byte value[32] = {};
+};
+
+[[nodiscard]] SHA256 hash_sha256(std::span<const std::byte> data);
+[[nodiscard]] SHA256 hash_sha256(std::string_view data);
+
+// -------------------------------------------------------------------------------------------------
+
+} // namespace security
+} // namespace libv
