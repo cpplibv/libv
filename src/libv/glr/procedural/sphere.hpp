de0851b4798dde614cf9e8754a06b0deac20591a
Császár Mátyás [Vader] <vaderhun@gmail.com>
2021-11-15 22:23:04 +0100

    libv.glr: Move legacy sphere implementations into libv as comment

--- src/libv/glr/procedural/sphere.hpp
+++ src/libv/glr/procedural/sphere.hpp
@@ -11,6 +11,8 @@ namespace glr {
 
 // -------------------------------------------------------------------------------------------------
 
+// --- Spherified Cube -----------------------------------------------------------------------------
+
 template <typename Precision = float, typename Position, typename Normal, typename Texture0, typename Index>
 void generateSpherifiedCube(uint32_t divisions, Position& position, Normal& normal, Texture0& texture0, Index& index) {
 	using F = Precision;
@@ -25,7 +27,6 @@ void generateSpherifiedCube(uint32_t divisions, Position& position, Normal& norm
 	normal.reserve(num_vertex);
 	texture0.reserve(num_vertex);
 	index.reserve(num_index);
-
 
 	divisions = std::max(divisions, 1u);
 	const auto step = F{1} / static_cast<F>(divisions);
@@ -97,7 +98,91 @@ void generateSpherifiedCube(uint32_t divisions, Position& position, Normal& norm
 	}
 }
 
-// -------------------------------------------------------------------------------------------------
+// --- Spherical Geodesic / Icosahedral Grid  ------------------------------------------------------
+
+//void subdivide(const vec3f& v1, const vec3f& v2, const vec3f& v3,
+//		std::vector<Vertex>& vertices, const uint32_t depth) {
+//	if (depth == 0) {
+//		vertices.emplace_back(v1);
+//		vertices.emplace_back(v3);
+//		vertices.emplace_back(v2);
+//		return;
+//	}
+//	const auto v12 = (v1 + v2).normalize();
+//	const auto v23 = (v2 + v3).normalize();
+//	const auto v31 = (v3 + v1).normalize();
+//	subdivide(v1, v12, v31, vertices, depth - 1);
+//	subdivide(v2, v23, v12, vertices, depth - 1);
+//	subdivide(v3, v31, v23, vertices, depth - 1);
+//	subdivide(v12, v23, v31, vertices, depth - 1);
+//}
+//
+//void sphere_geodesic(uint32_t subdiv) {
+//	std::vector<Vertex> vertices;
+//	std::vector<uint32_t> indices;
+//
+//	const float X = 0.525731112119133606f;
+//	const float Z = 0.850650808352039932f;
+//
+//	const vec3f vdata[12] = {
+//			{-X, 0.0, Z}, { X, 0.0, Z }, { -X, 0.0, -Z }, { X, 0.0, -Z },
+//			{ 0.0, Z, X }, { 0.0, Z, -X }, { 0.0, -Z, X }, { 0.0, -Z, -X },
+//			{ Z, X, 0.0 }, { -Z, X, 0.0 }, { Z, -X, 0.0 }, { -Z, -X, 0.0}
+//	};
+//
+//	int tindices[20][3] = {
+//			{0, 4, 1}, { 0, 9, 4 }, { 9, 5, 4 }, { 4, 5, 8 }, { 4, 8, 1 },
+//			{ 8, 10, 1 }, { 8, 3, 10 }, { 5, 3, 8 }, { 5, 2, 3 }, { 2, 7, 3 },
+//			{ 7, 10, 3 }, { 7, 6, 10 }, { 7, 11, 6 }, { 11, 0, 6 }, { 0, 1, 6 },
+//			{ 6, 1, 10 }, { 9, 0, 11 }, { 9, 11, 2 }, { 9, 2, 5 }, { 7, 2, 11}
+//	};
+//
+//	for (int i = 0; i < 20; i++)
+//		subdivide(vdata[tindices[i][0]], vdata[tindices[i][1]], vdata[tindices[i][2]], vertices, subdiv);
+//
+//	for (size_t i = 0; i < vertices.size(); ++i)
+//		indices.push_back(i);
+//}
+
+// --- Sphere UV -----------------------------------------------------------------------------------
+
+//void sphere_uv(float radius, uint32_t rings, uint32_t sectors) {
+//	std::vector<Vertex> vertices;
+//	std::vector<uint32_t> indices;
+//
+//	const float R = 1.f / (rings - 1);
+//	const float S = 1.f / (sectors - 1);
+//
+//	vertices.reserve(rings * sectors);
+//	indices.reserve(rings * sectors * 6);
+//
+//	for (uint32_t r = 0; r < rings; r++) {
+//		for (uint32_t s = 0; s < sectors; s++) {
+//			const auto x = std::cos(2 * iris::_PI * s * S) * std::sin(iris::_PI * r * R);
+//			const auto y = std::sin(2 * iris::_PI * s * S) * std::sin(iris::_PI * r * R);
+//			const auto z = std::sin(-iris::_PI_2 + iris::_PI * r * R);
+//
+//			vertices.emplace_back(
+//					vec3f(x, y, z),
+//					vec3(x, y, z),
+//					vec2(s * S, r * R)
+//					);
+//		}
+//	}
+//
+//	for (uint32_t r = 0; r < rings - 1; r++) {
+//		for (uint32_t s = 0; s < sectors - 1; s++) {
+//			indices.emplace_back(r * sectors + s);
+//			indices.emplace_back((r + 1) * sectors + s);
+//			indices.emplace_back((r + 1) * sectors + (s + 1));
+//			indices.emplace_back((r + 1) * sectors + (s + 1));
+//			indices.emplace_back(r * sectors + (s + 1));
+//			indices.emplace_back(r * sectors + s);
+//		}
+//	}
+//}
+
+// --- Fibonacci sphere vertices -------------------------------------------------------------------
 
 //std::vector<libv::vec3f> fibonacci_sphere_generator(libv::vec3f a, float radius, int sample_size) {
 //	std::vector<libv::vec3f> points;
