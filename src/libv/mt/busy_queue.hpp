811d05223a6f4b75e53b8233985621ce9b4065b8
Császár Mátyás [Vader] <vaderhun@gmail.com>
2022-04-18 00:39:25 +0200

    libv.mt: Add busy_queue as an initial concept to support busy waiting

---
+++ src/libv/mt/busy_queue.hpp
@@ -0,0 +1,55 @@
+// Project: libv.mt, File: src/libv/mt/busy_queue.hpp
+
+#pragma once
+
+// std
+#include <condition_variable>
+#include <mutex>
+#include <optional>
+#include <queue>
+#include <stop_token>
+
+
+namespace libv {
+namespace mt {
+
+// -------------------------------------------------------------------------------------------------
+
+template <typename T>
+class busy_queue {
+	std::queue<T> queue;
+	mutable std::mutex mutex;
+	mutable std::condition_variable_any cv;
+
+public:
+	template <typename... Args>
+	inline void emplace(Args&&... args) {
+		std::unique_lock lock(mutex);
+		queue.emplace(std::forward<Args>(args)...);
+		lock.unlock();
+		cv.notify_one();
+	}
+
+	[[nodiscard]] inline std::optional<T> try_wait_pop(std::stop_token stop_token) {
+		std::unique_lock lock(mutex);
+		const auto has_value = cv.wait(lock, stop_token, [&]{ return !queue.empty(); });
+
+		if (!has_value)
+			return std::nullopt;
+
+		std::optional<T> result{std::move(queue.front())};
+		queue.pop();
+
+		return result;
+	}
+
+	[[nodiscard]] inline size_t size() const noexcept {
+		std::unique_lock lock(mutex);
+		return queue.size();
+	}
+};
+
+// -------------------------------------------------------------------------------------------------
+
+} // namespace mt
+} // namespace libv
