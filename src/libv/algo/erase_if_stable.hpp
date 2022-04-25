2796895f342d1e62133a077e8d18c40b05c8428f
Bobula Dalma <almaspite98@gmail.com>
2022-04-25 15:20:24 +0200

    libv.algo: Add erase_if_stable_count

--- src/libv/algo/erase_if_stable.hpp
+++ src/libv/algo/erase_if_stable.hpp
@@ -15,6 +15,14 @@ constexpr inline void erase_if_stable(Container& c, Pred&& pred) {
 	c.erase(std::remove_if(c.begin(), c.end(), std::forward<Pred>(pred)), c.end());
 }
 
+template <typename Container, typename Pred>
+constexpr inline std::size_t erase_if_stable_count(Container& c, Pred&& pred) {
+	const auto it = std::remove_if(c.begin(), c.end(), std::forward<Pred>(pred));
+	const auto count = std::distance(it, c.end());
+	c.erase(it, c.end());
+	return count;
+}
+
 // -------------------------------------------------------------------------------------------------
 
 } // namespace libv
