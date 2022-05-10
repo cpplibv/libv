147457c7c93d8c3b1ebf109da65313df2c267d20
Császár Mátyás [Vader] <vaderhun@gmail.com>
2022-05-10 19:27:46 +0200

    res: Add new tree model and adjust white balance

---
+++ res/model/tree_cactus_a_03.lua
@@ -0,0 +1,12 @@
+
+model.scale(0.01)
+--model.scale_height_to(10)
+--model.rotate(0, 90, 90)
+
+model.regex_replace_textures("tree_cactus_a_03_trunk.diffuse.png", "../../res/model/tree_cactus_a_03_trunk.diffuse.png");
+model.regex_replace_textures("tree_cactus_a_03_thorns.diffuse.png", "../../res/model/tree_cactus_a_03_thorns.diffuse.png");
+
+--model.print_materials()
+--model.print_textures()
+----model.print_nodes()
+--model.print_model();
---
+++ res/model/tree_palm_02.lua
@@ -0,0 +1,12 @@
+
+model.scale(0.01)
+--model.scale_height_to(10)
+--model.rotate(0, 90, 90)
+
+model.regex_replace_textures("tree_palm_02_trunk.png", "../../res/model/tree_palm_02_trunk.diffuse.png");
+model.regex_replace_textures("tree_palm_02_leaf.png", "../../res/model/tree_palm_02_leaf.diffuse.png");
+
+--model.print_materials()
+--model.print_textures()
+----model.print_nodes()
+--model.print_model();
